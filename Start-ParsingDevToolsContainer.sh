5015a023849881fa1444d08b88b86120464364f3
Patryk Dziedzic <patrykdziedzic4@gmail.com>
2018-12-23 16:06:45 +0100

    Remove absolute paths hardcodes from container starting script

--- Start-ParsingDevToolsContainer.sh
+++ Start-ParsingDevToolsContainer.sh
@@ -1,1 +1,2 @@
-docker run --name parsing-dev-tools -t -d -v /Users/romandziedzic/Desktop/AUG/aug-compiler:/aug-compiler knordman/bison-flex-builder
+ScriptPath="$( cd "$( dirname "$0" )" >/dev/null && pwd )"
+docker run --name parsing-dev-tools -t -d -v "$ScriptPath"/aug-compiler:/aug-compiler knordman/bison-flex-builder
